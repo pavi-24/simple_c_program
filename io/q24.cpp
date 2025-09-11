75e36020d97b408411f3dc9688b101641708614a
PAVITHRAN S <pavithrand2405@gmail.com>
2025-09-11 14:57:58 +0530

    Add files via upload

---
+++ io/q24.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	int h,m,s;
+	scanf("%d%d%d",&h,&m,&s);
+	printf("%2d%2d%2d",h,m,s);
+	return 0;
+}
---
+++ io/q25.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int dr;
+	char ar,vi,ci;
+	scanf("%d%[-\n]s%[-\n]s%[-\n]s",&dr,&ar,&vi,&ci);
+	printf("%d\n%s\n%s\n%s",&dr,&ar,&vi,&ci);
+	return 0;
+}
